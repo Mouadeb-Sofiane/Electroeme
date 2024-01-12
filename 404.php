81feea72b4b8ff51ef12991f5ea4f32284ea1309
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-01-12 22:50:12 +0100

    3 e commit

--- 404.php
+++ 404.php
@@ -1,74 +1,68 @@
-
-<?php
-
-get_header(); // Inclure l'en-tête du thème
-?>
-
+<script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
 <body>
-
-    <div class="erreur">
-        <div class="oups">
-            <h1> Oups !</h1>
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/404Electroeme_img.png" alt="oups" class="error-image">
-        </div>
-        <p class="eror">Vous êtes un peu trop gourmand !</p>
-        <p class="eror">Revenez à l'accueil pour calmer votre appétit </p>
-        <a href="<?php echo home_url(); ?>" class="btn btn-primary">Retour à l'accueil</a>
-       
-
-    </div>
-
+  <div class="mainbox">
+    <div class="err">4</div>
+    <i class="far fa-question-circle fa-spin"></i>
+    <div class="err2">4</div>
+    <div class="msg">Peut-être que cette page a été déplacée ? Supprimée ? Est en quarantaine quelque part ? N'a jamais existé en premier lieu.?<p>Allons sur  <a href="https://electroeme.com/">la page d'accueil.</a></p></div>
+  </div>
 </body>
-
-<?php get_footer(); ?>
 <style>
- 
+    body {
+  background-color: #D8BFD8;
+  font-family: 'Poppins', sans-serif;
+}
 
-    body {
-        background-image: url('<?php echo get_stylesheet_directory_uri(); ?>./img/boussole.jpg');
-        background-position: center;
-        background-repeat: no-repeat;
-    }
+.mainbox {
+  background-color: #D8BFD8;
+  margin: auto;
+  height: 600px;
+  width: 600px;
+  position: relative;
+}
 
-    h1 {
-        padding: 10px;        
-    }
-  
-   html, body {
-        margin: 0;
-        padding: 0;
-        height: 100%; /* Assurez-vous que le body occupe toute la hauteur */
-        width: 100%;
-        display: flex; /* Utilisez Flexbox */
-        justify-content: center; /* Centrage horizontal */
-        align-items: center; /* Centrage vertical */
-    }
+  .err {
+    color: #f2f2f2;
+    font-family: 'Poppins', sans-serif;
+    font-size: 11rem;
+    position:absolute;
+    left: 20%;
+    top: 8%;
+  }
 
-    .erreur {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-       
-        background-color: #fff;
-        width: 600px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-        border-radius: 10px;
-        margin: auto; /* Auto-margin pour le centrage */
-        padding-bottom: 20px;
-    }
-    .error-image {
-        width: 10px;
-    }
-   
-    .oups {
-        display: flex;
-        align-items: baseline;
-        padding-top: 10px;
-    }
-   
-    a {
-        text-decoration: none;
-    }
+.far {
+  position: absolute;
+  font-size: 8.5rem;
+  left: 42%;
+  top: 15%;
+  color: #ffffff;
+}
+
+ .err2 {
+    color: #ffffff;
+    font-family: 'Poppins', sans-serif;
+    font-size: 11rem;
+    position:absolute;
+    left: 68%;
+    top: 8%;
+  }
+
+.msg {
+    text-align: center;
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.6rem;
+    position:absolute;
+    left: 16%;
+    top: 45%;
+    width: 75%;
+  }
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
+a:hover {
+  text-decoration: underline;
+}
 </style>
-
--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@
     // Ajout du fichier style.css
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     // Ajout du fichier main.js
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true );
+    //wp_enqueue_script( 'main', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true );
   }
   add_action( 'wp_enqueue_scripts', 'thememonsite_script' );
 
